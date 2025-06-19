6e91d3f447ed655eeeff861af0531fbf6568ee5a
Ömer Çetiner <omer39ctnr@gmail.com>
2025-06-19 16:31:48 +0300

    ËFÅ

--- gercekSanalMagaza/sanal_magaza/lib/pages/urun_listesi_sayfasi.dart
+++ gercekSanalMagaza/sanal_magaza/lib/pages/urun_listesi_sayfasi.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:sanal_magaza/models/urun_model.dart';
+import 'package:sanal_magaza/pages/login.dart';
 import 'package:sanal_magaza/services/urun_servisi.dart';
-import 'package:sanal_magaza/widgets/arama_cubugu.dart';
 import 'package:sanal_magaza/widgets/filtreleme_alani.dart';
 import 'package:sanal_magaza/widgets/urun_kart.dart';
 import 'package:sanal_magaza/widgets/ust_menu.dart';
@@ -17,6 +18,7 @@ class UrunListesiSayfasi extends StatefulWidget {
 class _UrunListesiSayfasiState extends State<UrunListesiSayfasi> {
   final UrunServisi _urunServisi = UrunServisi();
   List<UrunModel> _urunler = [];
+  List<UrunModel> _tumUrunler = [];
   Map<int, bool> _seciliUrunler = {};
   String _aramaMetni = '';
   StokFiltreSecenekleri _seciliStokFiltre = StokFiltreSecenekleri.hepsi;
@@ -26,75 +28,79 @@ class _UrunListesiSayfasiState extends State<UrunListesiSayfasi> {
       _seciliUrunler.keys.where((id) => _seciliUrunler[id] == true).toSet();
   bool get _herhangiUrunSeciliMi => _seciliUrunIdleri.isNotEmpty;
 
-  @override
-  void initState() {
-    super.initState();
-    _urunleriGetirVeFiltrele();
-  }
-  
-  
-  Future<void> _urunleriGetirVeFiltrele() async {
+
+@override
+void initState() {
+  super.initState();
+  _urunlerGetir();
+}
+
+Future<void> _urunlerGetir() async {
+  setState(() {
     _yukleniyor = true;
-    _urunler = [];
-    try {
-      int? minAdet;
-      int? maxAdet;
-      _urunler = await _urunServisi.tumUrunleriGetir();
-
-      switch (_seciliStokFiltre) {
-        case StokFiltreSecenekleri.azStoklu:
-          maxAdet = 9;
-          break;
-        case StokFiltreSecenekleri.yeterliStoklu:
-          minAdet = 10;
-          break;
-        case StokFiltreSecenekleri.hepsi:
-          break;
-      }
-
-      final filtrelenmisUrunler = await _urunServisi.urunAraVeFiltrele(
-        _aramaMetni,
-        minAdet: minAdet,
-        maxAdet: maxAdet,
-      );
-
-      setState(() {
-        _urunler = filtrelenmisUrunler;
-
-        Map<int, bool> yeniSeciliDurum = {};
-        for (var urun in filtrelenmisUrunler) {
-          yeniSeciliDurum[urun.id] = _seciliUrunler[urun.id] ?? false;
-        }
-        _seciliUrunler = yeniSeciliDurum;
-        _yukleniyor = false;
-      });
-    } catch (e) {
-      print(
-          'HATA: Ürünler getirilirken veya filtrelenirken bir hata oluştu: $e');
-      setState(() {
-        _yukleniyor = false;
-      });
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Ürünler yüklenirken hata oluştu: $e')),
-      );
-    }
-  }
-  
-  void _aramaMetniDegisti(String aramaMetni) {
+  });
+  try {
+    _tumUrunler = await _urunServisi.tumUrunleriGetir();
+    _filtreleVeGuncelle();
+  } catch (e) {
     setState(() {
-      _aramaMetni = aramaMetni;
+      _yukleniyor = false;
     });
-    _urunleriGetirVeFiltrele();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Ürünler yüklenirken hata oluştu: $e')),
+    );
   }
+}
 
-  void _stokFiltreDegisti(StokFiltreSecenekleri? yeniFiltre) {
-    if (yeniFiltre != null) {
-      setState(() {
-        _seciliStokFiltre = yeniFiltre;
-      });
-      _urunleriGetirVeFiltrele();
-    }
+void _filtreleVeGuncelle() {
+  int? minAdet;
+  int? maxAdet;
+  switch (_seciliStokFiltre) {
+    case StokFiltreSecenekleri.azStoklu:
+      maxAdet = 9;
+      break;
+    case StokFiltreSecenekleri.yeterliStoklu:
+      minAdet = 10;
+      break;
+    case StokFiltreSecenekleri.hepsi:
+      break;
   }
+  final filtrelenmisUrunler = _tumUrunler.where((urun) {
+    final ad = urun.ad.toLowerCase();
+    final aranan = _aramaMetni.toLowerCase();
+    final stok = urun.stokNumarasi;
+    bool stokDurumu = true;
+    if (minAdet != null && stok < minAdet) stokDurumu = false;
+    if (maxAdet != null && stok > maxAdet) stokDurumu = false;
+    return ad.contains(aranan) && stokDurumu;
+  }).toList();
+
+  setState(() {
+    _urunler = filtrelenmisUrunler;
+    Map<int, bool> yeniSeciliDurum = {};
+    for (var urun in filtrelenmisUrunler) {
+      yeniSeciliDurum[urun.id] = _seciliUrunler[urun.id] ?? false;
+    }
+    _seciliUrunler = yeniSeciliDurum;
+    _yukleniyor = false;
+  });
+}
+
+void _aramaMetniDegisti(String aramaMetni) {
+  setState(() {
+    _aramaMetni = aramaMetni;
+  });
+  _filtreleVeGuncelle();
+}
+
+void _stokFiltreDegisti(StokFiltreSecenekleri? yeniFiltre) {
+  if (yeniFiltre != null) {
+    setState(() {
+      _seciliStokFiltre = yeniFiltre;
+    });
+    _filtreleVeGuncelle();
+  }
+}
 
   void _gitButonunaBasildi() async {
     final List<UrunModel> gonderilecekUrunler =
@@ -108,10 +114,7 @@ class _UrunListesiSayfasiState extends State<UrunListesiSayfasi> {
               UrunYonetimSayfasi(seciliUrunler: gonderilecekUrunler),
         ),
       );
-
-      if (result == true) {
-        _urunleriGetirVeFiltrele();
-      }
+      _urunlerGetir();
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Lütfen önce ürün seçin!')),
@@ -126,17 +129,18 @@ class _UrunListesiSayfasiState extends State<UrunListesiSayfasi> {
   }
 
   void _yazdir() {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Yazdırma özelliği entegre edilecek.')),
-    );
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Login(),
+        ));
   }
 
-  void _duzenle() {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Düzenleme özelliği entegre edilecek.')),
-    );
+  void _duzenle() async{
+    _urunlerGetir();
   }
-  
+
+  TextEditingController myController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -147,7 +151,27 @@ class _UrunListesiSayfasiState extends State<UrunListesiSayfasi> {
       ),
       body: Column(
         children: [
-          AramaCubugu(onAramaYapildi: _aramaMetniDegisti),
+          Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: TextField(
+        controller: myController,
+        onChanged: (value) {
+               _aramaMetniDegisti(value);
+            },
+        decoration: InputDecoration(
+          labelText: 'Ürün Ara',
+          hintText: 'Ürün adı veya stok numarası ile ara',
+          prefixIcon: const Icon(Icons.search, color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide.none,
+          ),
+          filled: true,
+          fillColor: Colors.grey[100],
+          contentPadding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 16.0),
+          
+        ),
+        )),
           FiltrelemeAlani(
             seciliFiltre: _seciliStokFiltre,
             onFiltreDegisti: _stokFiltreDegisti,
